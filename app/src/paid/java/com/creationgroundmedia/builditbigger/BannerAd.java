2de6aee5487602b99c9c484a7936c71a1708b934
geocohn <geo@creationgroundmedia.com>
2016-03-07 08:59:48 -0800

    Add a banner ad to the free flavor

---
+++ app/src/paid/java/com/creationgroundmedia/builditbigger/BannerAd.java
@@ -0,0 +1,14 @@
+package com.creationgroundmedia.builditbigger;
+
+import android.view.View;
+
+
+/**
+ * Created by George Cohn on 3/7/16.
+ * This is a stub that does not display a banner ad in the paid version
+ */
+public class BannerAd {
+
+    public void show(View view) {
+    }
+}
